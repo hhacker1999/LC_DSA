99cbc4309662520ff8befe8ee768771bd34ce4b6
Harsh Kumar <harshkumar@Harshs-MacBook-Air.local>
2022-05-17 23:52:36 +0530

    fix for some usecases

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,10 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        lengthEqual: bool = len(s) == len(t)
-        contains = True
-        for i in s:
-            if i not in t:
-                contains = False
-                break
-        isAnagram: bool = contains and len
-        return isAnagram
+        if len(s) != len(t):
+            return False
+        mapS, mapT = {} , {}
+        for i in range(len(s)):
+            mapS[s[i]] = mapS.get(s[i], 0) + 1
+            mapT[t[i]] = mapT.get(t[i], 0) + 1
+        return mapS == mapT
+
